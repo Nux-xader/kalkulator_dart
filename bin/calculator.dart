12f448a51e26de687b752ce36e4e2392189de955
Nux-xader <sumgmng@gmail.com>
2021-12-23 16:09:16 +0700

    create input, and clear console fuction

--- bin/calculator.dart
+++ bin/calculator.dart
@@ -1,5 +1,23 @@
 import 'dart:io';
 
-void main(List<String> arguments) {
 
+void clr() {
+  if(Platform.isWindows) {
+    // not tested, I don't have Windows
+    // may not to work because 'cls' is an internal command of the Windows shell
+    // not an executeable
+    print(Process.runSync("cls", [], runInShell: true).stdout); 
+  } else {
+    print(Process.runSync("clear", [], runInShell: true).stdout);
+  }
+}
+
+void main(List<String> arguments) {
+  clr();
+  print("1. Penjumlahan");
+  print("2. Pengrangan");
+  print("3. Perkalian");
+  print("4. Pembagian");
+  stdout.write("Pilih : ");
+  String choice = stdin.readLineSync();
 }
